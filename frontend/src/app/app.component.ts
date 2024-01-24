7a370fe86e41d532062cd09f2b57ec16ec1072e0
MJoDev <mjzdo2020@gmail.com>
2024-01-24 07:21:51 -0400

    solucionar error

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from './services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,11 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent {
   title = 'client';
-  constructor( public authService: AuthService){
+  constructor( public authService: AuthService, private router: Router){
    
+  }
+
+  redirectToInicioSesion() {
+    this.router.navigate(['/signin']);
   }
 }
--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -25,6 +25,6 @@ export class AuthService {
   }
   logOut(){
     localStorage.removeItem('token');
-    this.router.navigate(['/login']);
+    this.router.navigate(['/signin']);
   }
 }
